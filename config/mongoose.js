fbc6da162fd343f89b57b51d451a97fbf2182dfc
amarmishra <amar.m130@gmail.com>
2023-04-12 12:02:15 +0530

    reverting back to original mongoose config file

--- config/mongoose.js
+++ config/mongoose.js
@@ -3,33 +3,19 @@ require('dotenv').config()
 const mongoose=require('mongoose')
 mongoose.set('strictQuery', true);
 
-let db;
-// Connecting to the database
-const connect = async function () {
-    return mongoose.connect(process.env.MONGODB_SERVER_URL,{useNewUrlParser: true, useUnifiedTopology: true,dbName:process.env.MONGODB_DATABASE_NAME})
-  };
+mongoose.connect(process.env.MONGODB_SERVER_URL,{dbName:process.env.MONGODB_DATABASE_NAME})
 
+const db=mongoose.connection
+db.on('error',(err)=>{
+    console.error.bind('Error:after connection',err)
+})
 
-(async ()=>{
-    try{
-        await connect()
-
-        db=mongoose.connection
-        db.on('error',(err)=>{
-            console.error.bind('Error:after connection',err)
-        })
-        
-        db.once('open',()=>{
-            console.log("Successfully connected to the database")
-        })
-    
-    }
-    catch(err){
-        console.log("Error while making initial connection::",err)
-    }
-})()
+db.once('open',()=>{
+    console.log("Successfully connected to the database")
+})
 
 module.exports=db
+
    
 
         
