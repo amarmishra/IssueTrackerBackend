8e3c91e94449c274e7625f092d9ec82aac52c231
amarmishra <amar.m130@gmail.com>
2023-04-12 10:37:34 +0530

    cleaned mongoose connection file

--- config/mongoose.js
+++ config/mongoose.js
@@ -3,41 +3,23 @@ require('dotenv').config()
 const mongoose=require('mongoose')
 mongoose.set('strictQuery', true);
 
-function connectToDb(){
+let db;
 
-        return new Promise((resolve, reject) => {
-                mongoose.connect(process.env.MONGODB_SERVER_URL).then(()=>{
-                    db=mongoose.connection;
-                    db.on('error',(err)=>{
-                        console.error.bind(console,'An error occured while connecting database')
-                    })
-                    db.once('open',()=>{
-                        console.log("Successfully connected to the database")
-                        return resolve(db)
-                    })
-                    
-                })
-                .catch(reject)
-               
-            })
+mongoose.connect(process.env.MONGODB_SERVER_URL).then(()=>{
 
-}
+    db=mongoose.connection;
 
-
-
-async function startServer(){
-    try{
-        const db=await connectToDb()
-    }
-    catch(err){
-        console.log(err)
-    }
+    db.on('error',(err)=>{
+        console.error.bind('Error:after connection',err)
+    })
     
-}
+    db.once('open',()=>{
+        console.log("Successfully connected to the database")
+    })
 
-try {
-    startServer()
-}
-catch(err){
-    console.log(err)
-}
+    module.exports=db
+        
+}).catch((err)=>{ console.log("Error while making initial connection::",err)})
+   
+
+
